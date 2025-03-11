3e868ae40b7d1a8f12a64a51e7288d4afc41c13d
Joostring <jonas.ostring@gmail.com>
2025-03-11 23:08:39 +0100

    Added rainzone

---
+++ Bygones/Assets/Scripts/AudioScripts/AmbientSoundZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainSound : MonoBehaviour // Author Jonas Östring
+{
+    [Tooltip("Area where to play the sound")]
+    public Collider area;
+    [Tooltip("GameObject to follow")]
+    public GameObject player; // The GameObject who hold the audiolistener
+    
+    
+    void Update()
+    {
+        Vector3 closestPoint = area.ClosestPoint(player.transform.position);
+        transform.position = closestPoint;
+    }
+}
