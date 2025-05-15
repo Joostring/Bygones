b589e4ee0e1e6b2b9e1d9d01483de7f31ac3ec27
Joostring <jonas.ostring@gmail.com>
2025-05-15 15:57:24 +0200

    4

--- Bygones/Bygones/Assets/Scripts/StuffToExport/ObjectSanityLoss.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectSanityLoss : MonoBehaviour
-{
-    [SerializeField] public float amountSanityLoss = 10;
-    [SerializeField] public bool hasTriggerdSanityLoss = false;
-}
