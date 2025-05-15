d7795cf41eaf5f69f53b19a4ae52f9d1d1da0368
Keirenwall <Keirenwall00@gmail.com>
2025-05-15 09:35:37 +0200

    Nyaste versionen
    Push 2 av 4

---
+++ Bygones/Bygones/Assets/Keiren Export 2025-05-09/InventorySlotHover.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlotHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject currentItem;
+    public GameObject hoverBox;       
+    public TMP_Text hoverText;        
+
+    public string defaultText = "";
+
+    private void Start()
+    {
+        hoverBox.SetActive(false); 
+        hoverText.text = "";
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentItem != null)
+        {
+            hoverBox.SetActive(true);
+
+            
+            string infoText = GetHoverText(currentItem.name);
+            hoverText.text = infoText;
+        }
+        else
+        {
+            hoverBox.SetActive(false);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hoverText.text = ""; 
+        hoverBox.SetActive(false); 
+    }
+
+    private string GetHoverText(string itemName)
+    {
+        switch (itemName)
+        {
+            case "Key_Decorative_Inspect": return "Decorative Key" ;
+            case "Key_Gen_Inspect": return "Generator key";
+            case "Key_Office_Inspect": return "Office key" ;
+            case "Key_Front_Inspect": return "Front door key" ;
+            case "Flashlight_Inspect": return "Flashlight" ;
+            case "Note_1_Inspect": return "Lawyer's Note";
+            case "Key_Gate_Inspect": return "Gate key";
+            case "Matches_Inspect": return "Matchbox";
+            case "Newspaper_Inspect": return "Newspaper article";
+            default: return null;;
+        }
+    }
+}
---
+++ Bygones/Bygones/Assets/Keirens Export 20250403/Scripts/CursorManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    public class CursorManager : MonoBehaviour
+    {
+        public static CursorManager instance;
+
+        public Sprite defaultCursor;
+        public Sprite lockedCursor;
+        public Sprite doorCursor;
+        public Sprite pickupCursor;
+
+        private UnityEngine.UI.Image img;
+        public float maxDistance = 5f; // Maximum distance to check
+        public Camera cam; // Assign the player's camera
+        public GameObject player; // Reference to the player
+        public LayerMask interactableLayer; // Assign this to the "Interactable" layer in the Inspector
+
+        void Awake()
+        {
+            instance = this;
+            img = GetComponent<UnityEngine.UI.Image>();
+        }
+
+        void Update()
+        {
+            CursorHint();
+        }
+
+        void CursorHint()
+        {
+            if (player == null || cam == null) return;
+
+            // Cast a ray from the center of the screen
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                // Check if the object is on the "Interactable" layer
+                if (((1 <<hit.collider.gameObject.layer) & interactableLayer) != 0)
+                {
+                    img.rectTransform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    SetCursorToPickup();
+                Debug.Log("HandCursor");
+            }
+                else if (hit.collider.CompareTag("Locked"))
+                {
+                    img.rectTransform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    SetCursorToLocked();
+                Debug.Log("LockedCursor");
+            }
+                else if (hit.collider.CompareTag("Open"))
+                {
+                    img.rectTransform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    SetCursorToDoor();
+                Debug.Log("DoorCursor");
+            }
+                else
+                {
+                    img.rectTransform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+                    SetCursorToDefault();
+                Debug.Log("DefaultCursor");
+                }
+            }
+            else
+            {
+                img.rectTransform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+                SetCursorToDefault();
+            }
+        }
+
+        public void SetCursorToLocked()
+        {
+            img.sprite = lockedCursor;
+        }
+
+        public void SetCursorToDoor()
+        {
+            img.sprite = doorCursor;
+        }
+
+        public void SetCursorToDefault()
+        {
+            img.sprite = defaultCursor;
+        }
+
+        public void SetCursorToPickup()
+        {
+            img.sprite = pickupCursor;
+        }
+    }
+
