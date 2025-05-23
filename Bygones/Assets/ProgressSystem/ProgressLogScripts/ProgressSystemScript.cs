c06c30223c4f5250b63133c942196b3a0552d126
Joostring <jonas.ostring@gmail.com>
2025-05-23 13:47:09 +0200

    updte

--- Bygones/Assets/ProgressSystem/ProgressLogScripts/ProgressSystemScript.cs
+++ Bygones/Assets/ProgressSystem/ProgressLogScripts/ProgressSystemScript.cs
@@ -1,79 +1,4 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using TMPro;
-//using Unity.VisualScripting;
-//using UnityEngine;
 
-//public class ProgressSystem : MonoBehaviour
-//{
-
-//    //ADD SCRIPT TO PROGRESSSYSTEMOBJECT 
-
-//    [TextArea(3, 10)]
-//    public List<string> notes = new List<string>();
-//    public GameObject progressSystemObject;
-//    [SerializeField] private GameObject ProgressNoteData;
-//    public TMP_Text progressText;
-
-//    private bool isVisible = false;
-
-//    [SerializeField] private int waitTimer = 10;
-//    public void ToggleProgressViewUI()
-//    {
-
-//        if (progressSystemObject == null)
-//        {
-//            return;
-//        }
-
-//        if (progressText == null)
-//        {
-//            return;
-//        }
-
-//        isVisible = !isVisible;
-//        progressSystemObject.SetActive(isVisible);
-//        if (isVisible)
-//        {
-//            UpdateProgressText();
-//        }
-//    }
-
-//    public void HideProgressView()
-//    {
-//        if (progressSystemObject != null)
-//        {
-//            progressSystemObject.SetActive(false);
-//            isVisible = false;
-//        }
-//    }
-//    public void AddNote(string newNote)
-//    {
-
-//        Debug.Log("AddNote called with: " + newNote);
-//        if (string.IsNullOrWhiteSpace(newNote))
-//            return;
-
-//        notes.Add(newNote);
-//        UpdateProgressText();
-//        ShowNotification = true;
-
-//        //if (!isVisible) // OPEN LOGVIEW AUTOMATICALLY WHEN LOG IS ADDED
-//        //{
-//        //    ToggleProgressViewUI(); 
-//        //}
-//    }
-
-//    private void UpdateProgressText()
-//    {
-//        progressText.text = string.Join("\n\n", notes);
-//    }
-
-//    public bool ShowNotification { get; set; } = false;
-//    public bool IsVisible() => isVisible;
-
-
-//}
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -96,7 +21,7 @@ public class ProgressSystem : MonoBehaviour
     [SerializeField] private int waitTimer = 10;
     public void ToggleProgressViewUI()
     {
-        if (progressSystemObject == null) // Använd det omdöpta fältet
+        if (progressSystemObject == null) 
         {
             return;
         }
@@ -107,7 +32,7 @@ public class ProgressSystem : MonoBehaviour
         }
 
         isVisible = !isVisible;
-        progressSystemObject.SetActive(isVisible); // Använd det omdöpta fältet
+        progressSystemObject.SetActive(isVisible); 
         if (isVisible)
         {
             UpdateProgressText();
@@ -116,9 +41,9 @@ public class ProgressSystem : MonoBehaviour
 
     public void HideProgressView()
     {
-        if (progressSystemObject != null) // Använd det omdöpta fältet
+        if (progressSystemObject != null) 
         {
-            progressSystemObject.SetActive(false); // Använd det omdöpta fältet
+            progressSystemObject.SetActive(false); 
             isVisible = false;
         }
     }
